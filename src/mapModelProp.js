12e2dbfb808230ec64afc4822947adc00e913ed7
Eyas Ranjous <eyas.ranjous@gmail.com>
2020-05-19 22:59:22 -0500

    add mapModelProp

---
+++ src/mapModelProp.js
@@ -0,0 +1,45 @@
+const Ajv = require('ajv');
+const mapModel = require('./mapModel');
+const modelPropMapSchema = require('./schemas/modelPropMap');
+
+const mapModelProp = ($, modelPropMap) => {
+  const ajv = new Ajv();
+  if (!ajv.validate(modelPropMapSchema, modelPropMap)) {
+    throw new Error(ajv.errors.map((e) => e.message).join(', '));
+  }
+
+  const { type: propType } = modelPropMap;
+
+  if (propType === 'model') return mapModel($, modelPropMap.model);
+
+  const cast = (value) => {
+    if (propType === 'number') return +value;
+    if (propType === 'boolean') return !!value;
+    return value;
+  };
+
+  const { selector: { path, data } } = modelPropMap;
+  const element = $(path);
+
+  if (element.length === 0) return null;
+
+  if (data) {
+    return cast(element.data(data));
+  }
+
+  if (element.prop('tagName').toLowerCase() === 'input') {
+    return cast(element.val());
+  }
+
+  const value = element
+    .clone()
+    .children()
+    .remove()
+    .end()
+    .text()
+    .trim();
+
+  return cast(value);
+};
+
+module.exports = mapModelProp;
---
+++ test/mapModelProp.test.js
@@ -0,0 +1,71 @@
+const { assert } = require('chai');
+const { JSDOM } = require('jsdom');
+const jquery = require('jquery');
+const mapModelProp = require('../src/mapModelProp');
+
+describe('.mapModelProp(jQuery, modelPropMap)', () => {
+  it('throw an error if prop data map is not a valid scheme', () => {
+    const $ = jquery(new JSDOM('<div></div>').window);
+    assert.throw(
+      () => mapModelProp($, {}),
+      Error,
+      'should have required property \'type\''
+    );
+
+    assert.throw(
+      () => mapModelProp($, { type: 'string' }),
+      Error,
+      'should have required property \'.selector\''
+    );
+
+    assert.throw(
+      () => mapModelProp($, { type: 'model' }),
+      Error,
+      'should have required property \'.model\''
+    );
+
+    assert.throw(
+      () => mapModelProp($, { type: 'string', selector: {} }),
+      Error,
+      'should have required property \'path\''
+    );
+  });
+
+  it('map a prop from a text element', () => {
+    const $ = jquery(new JSDOM('<p id="test">123</p>').window);
+    const modelPropMap = {
+      type: 'number',
+      selector: {
+        path: '#test'
+      }
+    };
+    assert.strictEqual(mapModelProp($, modelPropMap), 123);
+  });
+
+  it('map a prop from an input element', () => {
+    const $ = jquery(
+      new JSDOM('<input class="test" type="text" value="test" />').window
+    );
+    const modelPropMap = {
+      type: 'string',
+      selector: {
+        path: '.test'
+      }
+    };
+    assert.strictEqual(mapModelProp($, modelPropMap), 'test');
+  });
+
+  it('map a prop from a data attribute', () => {
+    const $ = jquery(
+      new JSDOM('<p id="test" data-val="test-data"></p>').window
+    );
+    const modelPropMap = {
+      type: 'string',
+      selector: {
+        path: '#test',
+        data: 'val'
+      }
+    };
+    assert.strictEqual(mapModelProp($, modelPropMap), 'test-data');
+  });
+});
