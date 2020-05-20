c599f230be899ced6fa87215c63e18a3a070fafe
Eyas Ranjous <eyas.ranjous@gmail.com>
2020-05-20 00:36:49 -0500

    use chai.assert

--- test/fetchDom.test.js
+++ test/fetchDom.test.js
@@ -1,11 +1,9 @@
-const { expect } = require('chai');
+const { assert } = require('chai');
 const fetchDom = require('../src/fetchDom');
 
 describe('.fetchDom(url)', () => {
   it('fetchs the dom content of a page and resolve with jQuery', () => (
     fetchDom('https://www.lipsum.com')
-      .then(($) => (
-        expect($('#Inner h1:first').text()).to.equal('Lorem Ipsum'))
-      )
+      .then(($) => assert.equal($('#Inner h1:first').text(), 'Lorem Ipsum'))
   ));
 });
