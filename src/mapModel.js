5740a5c4a46daed3845043405fbf2002fd03e4e8
Eyas Ranjous <eyas.ranjous@gmail.com>
2020-05-20 00:31:49 -0500

    add mapModel

---
+++ src/mapModel.js
@@ -0,0 +1,32 @@
+/**
+ * dom-to-model
+ * @copyright 2020 Eyas Ranjous <eyas.ranjous@gmail.com>
+ * @license ISC
+ */
+
+const Ajv = require('ajv');
+const mapModelProp = require('./mapModelProp');
+const modelMapSchema = require('./schemas/modelMap');
+
+/**
+ * @description gets a model prop value from dom
+ * @param {jQuery} $
+ * @param {object} modelMap
+ * @return {object} the mapped model
+ */
+const mapModel = ($, modelMap) => {
+  const ajv = new Ajv();
+  if (!ajv.validate(modelMapSchema, modelMap)) {
+    throw new Error(ajv.errors.map((e) => e.message).join(', '));
+  }
+
+  const model = {};
+
+  Object.entries(modelMap.props).forEach(([propName, propMap]) => {
+    model[propName] = mapModelProp($, propMap);
+  });
+
+  return model;
+};
+
+module.exports = mapModel;
