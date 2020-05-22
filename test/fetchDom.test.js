c839cc832a380f4c0d9b9d34e186556b9e0cd6ba
Eyas Ranjous <eyas.ranjous@gmail.com>
2020-05-22 02:18:22 -0500

    refactor mapModel

--- test/fetchDom.test.js
+++
@@ -1,9 +0,0 @@
-const { assert } = require('chai');
-const fetchDom = require('../src/fetchDom');
-
-describe('.fetchDom(url)', () => {
-  it('fetchs the dom content of a page and resolve with jQuery', () => (
-    fetchDom('https://www.lipsum.com')
-      .then(($) => assert.equal($('#Inner h1:first').text(), 'Lorem Ipsum'))
-  ));
-});
