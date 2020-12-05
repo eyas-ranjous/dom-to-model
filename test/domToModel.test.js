816b724f8890865f9f6f428796f5d244acc95e31
Eyas Ranjous <eyas.ranjous@gmail.com>
2020-12-05 17:05:06 +0100

    rename

--- test/domToModel.test.js
+++ test/domToModel.test.js
@@ -1,28 +1,35 @@
 const { assert } = require('chai');
 const { domToModel } = require('../lib/domToModel');
 const imdbMovieMap = require('./fixtures/imdbMovieMap');
-const titlesReleasedOn2000Map = require('./fixtures/titlesReleasedOn2000');
+const imdbMovieTitlesMap = require('./fixtures/imdbMovieTitlesMap');
 const movieData = require('./fixtures/movie');
 
 describe('domToModel(modelMap[, url])', () => {
-  it('map dom to a model object', () => (
-    domToModel(imdbMovieMap)
-      .then((movie) => assert.deepEqual(movie, movieData))
-  )).timeout(15000);
+  it('map dom to a model object', async () => {
+    const url = 'http://www.imdb.com/title/tt0076759';
+    const movie = await domToModel(url, imdbMovieMap);
+    assert.deepEqual(movie, movieData);
+  }).timeout(15000);
 
-  it('map dom to a model collection', () => (
-    domToModel(titlesReleasedOn2000Map)
-      .then((movies) => assert.isAbove(movies.length, 0))
-  )).timeout(15000);
+  it('map dom to a model collection', async () => {
+    const url = 'https://www.imdb.com/search/title/?year=2000';
+    const movies = await domToModel(url, imdbMovieTitlesMap);
+    assert.isAbove(movies.length, 0);
+  }).timeout(15000);
 
   it('throw an error if url is missing', () => (
-    domToModel({}).catch((error) => (
-      assert.equal(error.message, 'missing model web page url')
+    domToModel().catch((error) => (
+      assert.equal(error.message, 'missing web page url')
     ))
   ));
 
+  it('throw an error model map is missing', () => (
+    domToModel('http://test_url')
+      .catch((error) => assert.equal(error.message, 'missing model map'))
+  ));
+
   it('throw an error model type is unknown', () => (
-    domToModel({ url: 'test', type: 'test', props: {} })
+    domToModel('http://test_url', { type: 'test', props: {} })
       .catch((error) => assert.equal(error.message, 'unknow model map type'))
   ));
 });
